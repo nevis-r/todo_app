d4d40facdd26c178e67af18fc5572c600ad10e54
nevis_2 <nevisrumi@gmail.com>
2025-12-06 15:18:02 -0500

    update(list): Update to add new item to list on button click

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
-function printItem() {
+function addItem() {
     let addInput = document.getElementById("addItem");
     let addInputText = addInput.value;
-    console.log(addInputText);
+    let toDoList = document.getElementById("toDoList");
+    let newItem = document.createElement("li");
+    newItem.textContent = addInputText
+    toDoList.appendChild(newItem)
 }
