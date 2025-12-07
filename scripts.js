2af239a2a458e4f33d697b1c5b3828ca3e9a96d7
Anthony Shea <agshea25@gmail.com>
2025-12-06 19:15:49 -0700

    fixed?;2;13~

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ function addTodo(){
     }
 }
 
-function updateTodoList(){
+function updateTodoList() {
     todoListUL.innerHTML = "";
     allTodos.forEach((todo, todoIndex)=>{
         todoItem = createTodoItem(todo, todoIndex);
@@ -27,17 +27,17 @@ function updateTodoList(){
 }
 
 function createTodoItem(todo, todoIndex){
-    const todoId = "todo-" +todoIndex;
+    const todoId = "todo-"+todoIndex;
     const todoLi = document.createElement("li");
     todoLI.className = "todo";
     todoLI.innerHTML = `
-        <input type="checkbox" id="todo-${todoId}">
-	<label class="custom-checkbox" for="todo-${todoId}">
+        <input type="checkbox" id="${todoId}">
+	<label class="custom-checkbox" for="${todoId}">
             <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
 	    <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
 	</svg>
 	</label>
-	<label for="todo-${todoId}" class="todo-text">
+	<label for="${todoId}" class="todo-text">
 		${todo}
 	</label>
 	<button class="delete-button">
@@ -45,6 +45,6 @@ function createTodoItem(todo, todoIndex){
 	    <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
 	    </svg>
 	</button>
-        `
+    `
     return todoLI;
 }
